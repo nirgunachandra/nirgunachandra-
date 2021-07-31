346b76f900e016a953614ea21a95a99b42d09c7f
nirgunachandra <88225908+nirgunachandra@users.noreply.github.com>
2021-07-31 12:37:54 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,126 @@
+var p5Inst = new p5(null, 'sketch');
+
+window.preload = function () {
+  initMobileControls(p5Inst);
+
+  p5Inst._predefinedSpriteAnimations = {};
+  p5Inst._pauseSpriteAnimationsByDefault = false;
+  var animationListJSON = {"orderedKeys":["bd593e4b-1950-4d28-89d4-84de52be288b","d347d8f7-3d31-4611-a5d4-ce6cbd609a9f"],"propsByKey":{"bd593e4b-1950-4d28-89d4-84de52be288b":{"name":"car","sourceUrl":"assets/api/v1/animation-library/gamelab/T3gaSeDCsA_YHvy7GieJSRtG4e1P8Ac1/category_vehicles/car_yellow.png","frameSize":{"x":71,"y":131},"frameCount":1,"looping":true,"frameDelay":2,"version":"T3gaSeDCsA_YHvy7GieJSRtG4e1P8Ac1","categories":["vehicles"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":71,"y":131},"rootRelativePath":"assets/api/v1/animation-library/gamelab/T3gaSeDCsA_YHvy7GieJSRtG4e1P8Ac1/category_vehicles/car_yellow.png"},"d347d8f7-3d31-4611-a5d4-ce6cbd609a9f":{"name":"car2","sourceUrl":"assets/api/v1/animation-library/gamelab/lHG1XFlqFup4wzdHby85uHkMnnYotG1g/category_vehicles/car_blue.png","frameSize":{"x":71,"y":131},"frameCount":1,"looping":true,"frameDelay":2,"version":"lHG1XFlqFup4wzdHby85uHkMnnYotG1g","categories":["vehicles"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":71,"y":131},"rootRelativePath":"assets/api/v1/animation-library/gamelab/lHG1XFlqFup4wzdHby85uHkMnnYotG1g/category_vehicles/car_blue.png"}}};
+  var orderedKeys = animationListJSON.orderedKeys;
+  var allAnimationsSingleFrame = false;
+  orderedKeys.forEach(function (key) {
+    var props = animationListJSON.propsByKey[key];
+    var frameCount = allAnimationsSingleFrame ? 1 : props.frameCount;
+    var image = loadImage(props.rootRelativePath, function () {
+      var spriteSheet = loadSpriteSheet(
+          image,
+          props.frameSize.x,
+          props.frameSize.y,
+          frameCount
+      );
+      p5Inst._predefinedSpriteAnimations[props.name] = loadAnimation(spriteSheet);
+      p5Inst._predefinedSpriteAnimations[props.name].looping = props.looping;
+      p5Inst._predefinedSpriteAnimations[props.name].frameDelay = props.frameDelay;
+    });
+  });
+
+  function wrappedExportedCode(stage) {
+    if (stage === 'preload') {
+      if (setup !== window.setup) {
+        window.setup = setup;
+      } else {
+        return;
+      }
+    }
+// -----
+
+var life = 0;
+var car1, car2, car3,car4;
+var boundary1, boundary2;
+var sam;
+
+  boundary1 = createSprite(190,120,420,3);
+  boundary2 = createSprite(190,260,420,3);
+  
+  sam = createSprite(20,190,13,13);
+  sam.shapeColor = "green";
+  
+  car1 = createSprite(100,130,10,10);
+  car1.setAnimation("car");
+  car1.scale=0.2;
+  car2 = createSprite(215,130,10,10);
+  car2.setAnimation("car");
+  car2.scale=0.2;
+  car3 = createSprite(165,250,10,10);
+  car3.setAnimation("car");
+  car3.scale=0.2;
+  car4 = createSprite(270,250,10,10);
+  car4.setAnimation("car");
+  car4.scale=0.2;
+ 
+//add the velocity to make the car move.
+car1.velocityY=8;
+car2.velocityY=8;
+car3.velocityY=-8;
+car4.velocityY=-8;
+
+function draw() {
+   background("white");
+  text("Lives: " + life,200,100);
+  strokeWeight(0);
+  fill("lightblue");
+  rect(0,120,52,140);
+  fill("yellow");
+  rect(345,120,52,140);
+  
+// create bounceoff function to make the car bounceoff the boundaries
+createEdgeSprites();
+car1.bounceOff(boundary1);
+car1.bounceOff(boundary2);
+car2.bounceOff(boundary1);
+car2.bounceOff(boundary2);
+car3.bounceOff(boundary1);
+car3.bounceOff(boundary2);
+car4.bounceOff(boundary1);
+car4.bounceOff(boundary2);
+//Add the condition to make the sam move left and right
+if(keyDown("left")){
+  sam.x=sam.x-3;
+}
+if(keyDown("right")){
+  sam.x=sam.x+3;
+}
+//Add the condition to reduce the life of sam if it touches the car.
+if(
+     sam.isTouching(car1)||
+     sam.isTouching(car2)||
+     sam.isTouching(car3)||
+     sam.isTouching(car4))
+  {
+    sam.x = 20;
+     sam.y = 190;
+     life = life + 1;
+  }
+
+  
+ drawSprites();
+}
+
+// -----
+    try { window.draw = draw; } catch (e) {}
+    switch (stage) {
+      case 'preload':
+        if (preload !== window.preload) { preload(); }
+        break;
+      case 'setup':
+        if (setup !== window.setup) { setup(); }
+        break;
+    }
+  }
+  window.wrappedExportedCode = wrappedExportedCode;
+  wrappedExportedCode('preload');
+};
+
+window.setup = function () {
+  window.wrappedExportedCode('setup');
+};
